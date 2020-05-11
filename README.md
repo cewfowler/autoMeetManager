4de243ca1209c2460e15be5be148123a1c86ac66
cewfowler <cewfowler@gmail.com>
2020-05-11 13:36:06 -0400

    Connect to google drive to read sheets
    Update credentials
    
    Connect to google sheets api
    
    Update README with info on connecting to google sheets api

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ pip3 install oauth2client
 ```
 - If there is an error using `pip3 install`, try `python3 -m pip install`.
 
+## Setup ##
+1. After creating the google form, open the response sheet. Click the *Share* button and go to "creds.json" found in the "google_sheets" directory. Copy the client_email (meetmanager@future-sonar-276916.iam.gserviceaccount.com) and paste it in the share box. Then click *Done*.
+
 ## Running the program ##
 1. Open the command line/terminal.
 2. Run `python3 folder_location/autoMeetManager/autoMeetManager.py` using the
@@ -20,4 +23,4 @@ correct path.
   - To navigate your file system from the command line use ```cd <subdirectory or path>``` to change directories, ```ls``` to check current subdirectories, and ```pwd``` to check the current directory.
 
 
-  [Getting Google Form Responses](https://pbpython.com/pandas-google-forms-part1.html)
+  [Getting Google Form Responses](https://www.youtube.com/watch?v=cnPlKLEGR7E)
--- google_sheets/sheets.py
+++ google_sheets/sheets.py
@@ -1,52 +1,24 @@
-from __future__ import print_function
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://spreadsheets.google.com/feeds']
 
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1-T6QP2lhpQb-ntFnub7sP9kdYaGcXkETgNAr9PfVqOM'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 
-def readSheet():
-    creds = None
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope);
+client = gspread.authorize(creds);
 
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+#def getDataFromSheet(sheet_name) {
 
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(success_message="Congrats! You're in")
+#}
 
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+sheet = client.open("FSU Fear The Spear Meet Signup  (Responses)").sheet1;
 
-    service = build('sheets', 'v4', credentials=creds)
+data = sheet.get_all_records();
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
+row = sheet.row_values(1);
+print(row);
 
-    if not values:
-        print('No data found.')
-    else:
-        print(row)
-
-if __name__ == '__main__':
-    main()
+for entry in data:
+    print("Timestamp: " + str( entry[row[0]] ));
+    print("Name: " + str( entry[row[1]] ));
+    break;
